2acbe3981a04c20ded7ff372f5e3430b4d6814f3
Lisa9W <lisa@m-release.com>
2017-04-15 13:55:55 +0200

    script simpeler gemaakt

--- www/js/transition_main.js
+++ www/js/transition_main.js
@@ -1,124 +1,19 @@
 $( document ).ready(function() {
-    $(".menu_button_1").click(function(){
+    $(".menu_button").click(function(){
         console.log("button click");
-        $(".menu_button_1").animate({
-            zIndex: '99'
-        });
-        $(".menu_button_1").animate({
-            transition: "0.3s",
-            width: '100%',
-            height: '100vh'
-        });
-        $(".menu_button_1 p").animate({
-            transition: '0.3s',
-            opacity: '0'
-        });
-        setTimeout(function() {
-            window.location.href = "profile.html";
-        }, 2000);
-    });
-
-    $(".menu_button_2").click(function(){
-        console.log("button click");
-        $(".menu_button_2").animate({
-            zIndex: '99'
-        });
-        $(".menu_button_2").animate({
-            transition: "0.3s",
+        $(this).css('z-index', '99');
+        $(this).animate({
             width: '100%',
             height: '100vh',
             marginTop: '0',
             marginLeft: '0'
         });
-        $(".menu_button_2 p").animate({
+        $(this).find('p').animate({
             transition: '0.3s',
             opacity: '0'
         });
         setTimeout(function() {
             window.location.href = "profile.html";
-        }, 2000);
-    });
-
-    $(".menu_button_3").click(function(){
-        console.log("button click");
-        $(".menu_button_3").animate({
-            zIndex: '99'
-        });
-        $(".menu_button_3").animate({
-            transition: "0.3s",
-            marginTop: '0',
-            width: '100%',
-            height: '100vh'
-        });
-        $(".menu_button_3 p").animate({
-            transition: '0.3s',
-            opacity: '0'
-        });
-        setTimeout(function() {
-            window.location.href = "profile.html";
-        }, 2000);
-    });
-
-    $(".menu_button_4").click(function(){
-        console.log("button click");
-        $(".menu_button_4").animate({
-            zIndex: '99'
-        });
-        $(".menu_button_4").animate({
-            transition: "0.3s",
-            width: '100%',
-            height: '100vh',
-            marginTop: '0',
-            marginLeft: '0'
-        });
-        $(".menu_button_4 p").animate({
-            transition: '0.3s',
-            opacity: '0'
-        });
-        setTimeout(function() {
-            window.location.href = "profile.html";
-        }, 2000);
-    });
-
-    $(".menu_button_5").click(function(){
-        console.log("button click");
-        $(".menu_button_5").animate({
-            zIndex: '99'
-        });
-        $(".menu_button_5").animate({
-            transition: "0.3s",
-            width: '100%',
-            height: '100vh',
-            marginTop: '0',
-            marginLeft: '0'
-        });
-        $(".menu_button_5 p").animate({
-            transition: '0.3s',
-            opacity: '0'
-        });
-        setTimeout(function() {
-            window.location.href = "profile.html";
-        }, 2000);
-    });
-
-    $(".menu_button_6").click(function(){
-        console.log("button click");
-        $(".menu_button_6").animate({
-            zIndex: '99'
-        });
-        $(".menu_button_6").animate({
-            transition: "0.3s",
-            width: '100%',
-            height: '100vh',
-            marginTop: '0',
-            marginLeft: '0'
-        });
-        $(".menu_button_6 p").animate({
-            transition: '0.3s',
-            opacity: '0'
-        });
-        setTimeout(function() {
-            window.location.href = "profile.html";
-        }, 2000);
+        }, 1000);
     });
 });
