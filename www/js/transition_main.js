3d69ee49685f3e85709af8d56ad47799a7286a83
Lisa9W <lisa@m-release.com>
2017-04-26 10:38:38 +0200

    Home script terug naar versie Michiel

--- www/js/transition_main.js
+++ www/js/transition_main.js
@@ -1,32 +1,136 @@
 $( document ).ready(function() {
-    $(".menu_button").click(function(){
+    $(".menu_button_profile").click(function(){
         console.log("button click");
-        $(this).css('z-index', '99');
-        $(this).animate({
+        //noinspection JSJQueryEfficiency
+        $(".menu_button_profile").animate({
+            zIndex: '99'
+        });
+        //noinspection JSJQueryEfficiency
+        $(".menu_button_profile").animate({
+            transition: "0.3s",
+            width: '100%',
+            height: '100vh'
+        });
+        $(".menu_button_profile p").animate({
+            transition: '0.3s',
+            opacity: '0'
+        });
+        setTimeout(function() {
+            window.location.href = "profile.html";
+        }, 2000);
+    });
+
+    $(".menu_button_cards").click(function(){
+        console.log("button click");
+        //noinspection JSJQueryEfficiency
+        $(".menu_button_cards").animate({
+            zIndex: '99'
+        });
+        //noinspection JSJQueryEfficiency
+        $(".menu_button_cards").animate({
+            transition: "0.3s",
             width: '100%',
             height: '100vh',
             marginTop: '0',
             marginLeft: '0'
-        }, 500);
-        $(this).find('p').animate({
+        });
+        $(".menu_button_cards p").animate({
             transition: '0.3s',
             opacity: '0'
-        }, 500);
+        });
+        setTimeout(function() {
+            window.location.href = "profile.html";
+        }, 2000);
+    });
 
-        console.log($(this).attr('class'));
-        var className = $(this).attr('class');
-        switch(className) {
-            case 'menu_button logout':
-                setTimeout(function () {
-                    window.location.href = "index.html";
-                }, 500);
-                break;
+    $(".menu_button_trade").click(function(){
+        console.log("button click");
+        //noinspection JSJQueryEfficiency
+        $(".menu_button_trade").animate({
+            zIndex: '99'
+        });
+        //noinspection JSJQueryEfficiency
+        $(".menu_button_trade").animate({
+            transition: "0.3s",
+            marginTop: '0',
+            width: '100%',
+            height: '100vh'
+        });
+        $(".menu_button_trade p").animate({
+            transition: '0.3s',
+            opacity: '0'
+        });
+        setTimeout(function() {
+            window.location.href = "profile.html";
+        }, 2000);
+    });
 
-            default:
-                setTimeout(function () {
-                    window.location.href = "profile.html";
-                }, 500);
-                break;
-        }
+    $(".menu_button_progress").click(function(){
+        console.log("button click");
+        //noinspection JSJQueryEfficiency
+        $(".menu_button_progress").animate({
+            zIndex: '99'
+        });
+        //noinspection JSJQueryEfficiency
+        $(".menu_button_progress").animate({
+            transition: "0.3s",
+            width: '100%',
+            height: '100vh',
+            marginTop: '0',
+            marginLeft: '0'
+        });
+        $(".menu_button_progress p").animate({
+            transition: '0.3s',
+            opacity: '0'
+        });
+        setTimeout(function() {
+            window.location.href = "profile.html";
+        }, 2000);
+    });
+
+    $(".menu_button_friends").click(function(){
+        console.log("button click");
+        //noinspection JSJQueryEfficiency
+        $(".menu_button_friends").animate({
+            zIndex: '99'
+        });
+        //noinspection JSJQueryEfficiency
+        $(".menu_button_friends").animate({
+            transition: "0.3s",
+            width: '100%',
+            height: '100vh',
+            marginTop: '0',
+            marginLeft: '0'
+        });
+        $(".menu_button_friends p").animate({
+            transition: '0.3s',
+            opacity: '0'
+        });
+        setTimeout(function() {
+            window.location.href = "profile.html";
+        }, 2000);
+    });
+
+    $(".menu_button_logout").click(function(){
+        console.log("button click");
+        //noinspection JSJQueryEfficiency
+        $(".menu_button_logout").animate({
+            zIndex: '99'
+        });
+        //noinspection JSJQueryEfficiency
+        $(".menu_button_logout").animate({
+            transition: "0.3s",
+            width: '100%',
+            height: '100vh',
+            marginTop: '0',
+            marginLeft: '0'
+        });
+        $(".menu_button_logout p").animate({
+            transition: '0.3s',
+            opacity: '0'
+        });
+        setTimeout(function() {
+            window.location.href = "profile.html";
+        }, 2000);
     });
 });
